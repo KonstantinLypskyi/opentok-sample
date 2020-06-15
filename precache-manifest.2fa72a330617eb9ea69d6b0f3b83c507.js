04a0ff1556ceab64a1b02ef1173d863960e26378
Konstantin Lypskyi <konstantinlypskyi@gmail.com>
2020-06-15 20:38:34 +0300

    Updates

---
+++ precache-manifest.2fa72a330617eb9ea69d6b0f3b83c507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7a6c05e477bb33fe49e1690ae9221e",
+    "url": "/opentok-sample/index.html"
+  },
+  {
+    "revision": "21a5eb4599c4b029e134",
+    "url": "/opentok-sample/static/css/main.78211eb0.chunk.css"
+  },
+  {
+    "revision": "fd61a6815f6dcc48844b",
+    "url": "/opentok-sample/static/js/2.5501f295.chunk.js"
+  },
+  {
+    "revision": "814e809ee66d7f8fc1ff934a7e1f624d",
+    "url": "/opentok-sample/static/js/2.5501f295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a5eb4599c4b029e134",
+    "url": "/opentok-sample/static/js/main.a0ce081b.chunk.js"
+  },
+  {
+    "revision": "d4ab1d804f1938424389",
+    "url": "/opentok-sample/static/js/runtime-main.bffc1c54.js"
+  }
+]);
